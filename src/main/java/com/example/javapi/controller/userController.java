9291ee6c55faf0e2e264bcff05feb8dc30a95d50
Florian <florianb04@gmail.com>
2021-12-10 16:53:12 +0100

    Writting Business Logic 1:08:03

--- src/main/java/com/example/javapi/controller/userController.java
+++ src/main/java/com/example/javapi/controller/userController.java
@@ -3,9 +3,7 @@ package com.example.javapi.controller;
 import com.example.javapi.Service.UserService;
 import com.example.javapi.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,5 +21,10 @@ public class userController {
     @GetMapping
     public List<User> getUsers() {
         return userService.getUsers();
+    }
+
+    @PostMapping
+    public void registerNewUser(@RequestBody User user){
+        userService.addNewUser(user);
     }
 }
