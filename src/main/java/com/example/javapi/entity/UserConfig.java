80eb52386e299826c5f5b514470f9fc0f44a3614
Florian <florianb04@gmail.com>
2021-12-10 15:16:01 +0100

    chapter Transient 58m50 OK

---
+++ src/main/java/com/example/javapi/entity/UserConfig.java
@@ -0,0 +1,34 @@
+package com.example.javapi.entity;
+
+import com.example.javapi.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class UserConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner (UserRepository repository) {
+        return args -> {
+            User aaa = new User(
+                    "aaa",
+                    "AAA",
+                    "aaa@aaa;fr",
+                    "aaa"
+            );
+
+            User bbb = new User(
+                    "bbb",
+                    "BBB",
+                    "bbb@bbb;fr",
+                    "bbb"
+            );
+
+            repository.saveAll(List.of(aaa, bbb)
+            );
+        };
+    }
+}
